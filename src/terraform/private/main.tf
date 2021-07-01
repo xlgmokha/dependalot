33eabf2ea1ecf894e3b526ff5b040c04098cbcaa
mo khan <mo@mokhan.ca>
2021-07-01 12:16:49 -0600

    chore: use pessimistic constraint

--- src/terraform/private/main.tf
+++ src/terraform/private/main.tf
@@ -9,8 +9,5 @@ terraform {
 
 module "s3-webapp" {
   source  = "app.terraform.io/example-org-5d3190/s3-webapp/aws"
-  name   = var.name
-  region = var.region
-  prefix = var.prefix
-  version = "1.0.0"
+  version = "~> 1.0"
 }
