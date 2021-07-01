7af717ced0b3f8c9355cf698b264258f89137324
mo khan <mo@mokhan.ca>
2021-07-01 12:09:05 -0600

    feat: add terraform project

---
+++ src/terraform/private/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+module "s3-webapp" {
+  source  = "app.terraform.io/example-org-5d3190/s3-webapp/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
+}
